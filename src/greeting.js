ad6500f41c50863f861c4d3cdc43853422ef0660
thegolumehra <golucodes@gmail.com>
2023-07-09 12:09:36 +0530

    added greeets

--- src/greeting.js
+++ src/greeting.js
@@ -1,29 +1,39 @@
 import { useEffect, useState } from "react";
 
 function Greeting() {
+    // eslint-disable-next-line no-unused-vars
     const [randomNumber,setRandomNumber] = useState(
         Math.floor(Math.random()*5 +1)
     );
+    const [greeting,setGreeting] = useState('')
     useEffect(()=>{
-        console.log('Random Greeting')
-    },[]);
+        // eslint-disable-next-line default-case
+        switch(randomNumber) {
+            case 1:
+                setGreeting('Hello');
+                break;
+            case 2:
+                setGreeting('Bonjour');
+                break;
+            case 3:
+                setGreeting('Hola');
+                break;
+            case 4:
+                setGreeting('Guten Tag');
+                break;
+            case 5:
+                setGreeting('Ciao');
+                break;
+            default:
+                setGreeting('Hello');
+                break;
+        }
+    },[randomNumber])
 
     return (
         <div className="greeting">
             <h1>Random Greeting</h1>
-            {randomNumber === 1 ? (
-                <h2 className="message">Hello</h2>
-            ) : randomNumber===2 ? (
-                <h2 className="message">Howdy</h2>
-            ) : randomNumber===3? (
-                <h2 className="message">Hi</h2>
-            ) : randomNumber===4 ? (
-                <h2 className="message">Hey</h2>
-            ) : randomNumber===5 ? (
-                <h2 className="message">yo</h2>
-            )  : (
-                <h2 className="message">Salutation</h2>
-            )}
+            <h2 className="message">{greeting}</h2>
         </div>
     );
 };
